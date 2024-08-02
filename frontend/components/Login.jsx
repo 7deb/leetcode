c05bc0fa33ced00dc78bbdf343e6d0dfa0909ee7
ReaperOAK <oaak78692@gmail.com>
2024-08-02 16:00:34 +0530

    login component complete, add functionality

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,59 +1,62 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
+import { Container, Form, Button, Image } from 'react-bootstrap';
+import { FaEye, FaGoogle, FaGithub, FaFacebook } from 'react-icons/fa';
+import '../styles/Login.css';
 
-const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+const LoginPage = () => {
+        const [showPassword, setShowPassword] = useState(false);
+      
+        const togglePasswordVisibility = () => {
+          setShowPassword(!showPassword);
+        }; 
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
+    
+        const { loading, login } = useLogin();
+    
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            await login(username, password);
+        };
 
-    const { loading, login } = useLogin();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        await login(username, password);
-    };
-
-    return (
-        <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
-            <div className="p-6 h-full w-full bg-gray-900 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
-                <h1 className="text-3xl font-semibold text-center text-gray-300">
-                    Login
-                </h1>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label className="label p-2">
-                            <span className="text-base label-text">Username</span>
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Username"
-                            className="w-full input text-center input-bordered h-10"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="sign-in-section">              
+                <div className='leetcode-login-header'>
+                  <Image alt="LeetCode" src="../images/LClogo-h4.svg" className="logo mb-4"/>
+                </div>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="formUsername">
+                    <Form.Control type="text" placeholder="Username or E-mail" autoComplete="username"  className='mb-4' value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                  </Form.Group>
+                  <Form.Group controlId="formPassword" className="position-relative">
+                    <Form.Control type={showPassword ? "text" : "password"} placeholder="Password" autoComplete="current-password" className='mb-4' value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    <Button variant="link" onClick={togglePasswordVisibility} className="position-absolute top-50 end-0 translate-middle-y">
+                      <FaEye />
+                    </Button>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100 mb-3" id='sign-in'>Sign In</Button>
+                </Form>
+                <div className="d-flex justify-content-between a-and-span">
+                  <a href="/accounts/password/reset/">Forgot Password?</a>
+                  <a href="/signup">Sign Up</a>
+                </div>
+                <div className="my-4 text-center other-login-options-prime">
+                  <p>or you can sign in with</p>
+                  <center>
+                    <div className="d-flex other-login-options">
+                      <a href="https://leetcode.com/accounts/google/login/?next=%2F" className='google'><FaGoogle /></a>
+                      <a href="https://leetcode.com/accounts/github/login/?next=%2F" className='github'><FaGithub /></a>
+                      <a href="https://leetcode.com/accounts/facebook/login/?next=%2F" className='facebook'><FaFacebook /></a>
                     </div>
-                    <div>
-                        <label className="label ">
-                            <span className="text-base label-text">Password</span>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            className="w-full input input-bordered h-10 text-center"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <Link to="/signup" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Don't have an account?</Link>
-                    <div>
-                        <button className='btn btn-block btn-sm mt-2' disabled={loading}>
-                            {loading ? <span className='loading loading-spinner '></span> : "Login"}
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+                  </center>
+                </div>
+              </div>
+            </div>          
+  );
 };
 
 export default LoginPage;
