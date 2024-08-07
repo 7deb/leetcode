d9a3d1de90372782ca0d931eaf280243c6e374a9
ReaperOAK <oaak78692@gmail.com>
2024-08-07 22:37:50 +0530

    home page almost complete, more refining to be done soon

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -25,16 +25,34 @@ const Home = () => {
       </Row>
       <Row className="explore-section">
         <Col md={6}>
-          <h2 className="explore-title">Start Exploring</h2>
+          <h2 className="explore-title">Start Exploring            <img src="../images/startExploring.png" width={50}/></h2>
           <p className="explore-subtitle">
             Explore is a well-organized tool that helps you get the most out of LeetCode by providing structure to guide your progress towards the next step in your programming career.
           </p>
           <a href='' className='explore-button'>Get Started</a>
         </Col>
         <Col md={6} className="explore-images-container cards">
-          <img src="../images/c3.png" alt="Hero" className="explore-image cards-1"/>
-          <img src="../images/c2.png" alt="Hero" className="explore-image cards-2"/>
-          <img src="../images/c1.png" alt="Hero" className="explore-image cards-3"/>  
+          <img src="../images/c3.png" alt="" className="explore-image cards-1"/>
+          <img src="../images/c2.png" alt="" className="explore-image cards-2"/>
+          <img src="../images/c1.png" alt="" className="explore-image cards-3"/>  
+        </Col>
+      </Row>
+      <Row className="qc4-section">
+        <Col md={6} className='qc4-section-inner' style={{borderRight:"3px solid white"}}>
+          {/* <img src="../images/startExploring.png" width={50}/> */}
+          <h2 className="qc4-title">Start Exploring</h2>
+          <p className="qc4-subtitle">
+            Explore is a well-organized tool that helps you get the most out of LeetCode by providing structure to guide your progress towards the next step in your programming career.
+          </p>
+          <a href='' className='qc4-button'>Get Started</a>
+        </Col>
+        <Col md={6} className='qc4-section-inner'>
+        {/* <img src="../images/startExploring.png" width={50}/> */}
+        <h2 className="qc4-title">Start Exploring</h2>
+          <p className="qc4-subtitle">
+            Explore is a well-organized tool that helps you get the most out of LeetCode by providing structure to guide your progress towards the next step in your programming career.
+          </p>
+          <a href='' className='qc4-button'>Get Started</a>  
         </Col>
       </Row>
     </Container>
