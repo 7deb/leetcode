5fcdd497c3bdac7dd4439595bcefe6294d521fda
ReaperOAK <oaak78692@gmail.com>
2024-08-12 21:10:07 +0530

    hehe

--- frontend/pages/Explore.jsx
+++ frontend/pages/Explore.jsx
@@ -5,41 +5,41 @@ import exploreCards from '../DS/exploreCards';
 import '../styles/Explore.css';
 
 const Explore = () => {
-  
-
   return (
     <Container className="my-4">
       <h2 className="mb-4">Featured</h2>
-      <Row xs={1} md={2} lg={3} className="g-4">
-        {exploreCards.map((course, index) => (
-          <Col key={index}>
-            <Card className="h-100 custom-card">
-              <Card.Body className="position-relative">
-                <Card.Title className="mb-4 fw-bold">
-                  {course.description}
-                </Card.Title>
-                <Card.Text className="fs-3 fw-bold">
+      <Row xs={1} md={2} lg={4} className="g-4">
+        {exploreCards
+          .filter((course) => course.section === "Featured")
+          .map((course, index) => (
+            <Col key={index}>
+              <Card className="custom-card">
+                <Card.Body className="position-relative">
+                <Card.Text className="fs-10">
+                {course.description}
+                  </Card.Text>
+                  <Card.Title className="mb-4 fs-3 fw-bold">
                   {course.title}
-                </Card.Text>
+                  </Card.Title>
+                </Card.Body>
+                <Card.Footer className="bg-white d-flex justify-content-between text-muted">
+                  <div>
+                    <strong>{course.chapters}</strong> Chapters
+                  </div>
+                  <div>
+                    <strong>{course.items}</strong> Items
+                  </div>
+                  <div>
+                    {course.progress}% 
+                  </div>
+                </Card.Footer>
                 <PlayCircle
-                  size={50}
-                  className="position-absolute bottom-0 end-0 play-icon"
-                />
-              </Card.Body>
-              <Card.Footer className="bg-white d-flex justify-content-between text-muted">
-                <div>
-                  <strong>{course.chapters}</strong> Chapters
-                </div>
-                <div>
-                  <strong>{course.items}</strong> Items
-                </div>
-                <div>
-                  {course.progress}% 
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
-        ))}
+                    size={50}
+                    className="position-absolute play-icon"
+                  />
+              </Card>
+            </Col>
+          ))}
       </Row>
     </Container>
   );
